e91b995062a88ace1e0ca79465e186852692430d
yuyong <1103023317@qq.com>
2018-07-24 14:43:51 +0800

    demo修改

--- src/main/java/cn/tedu/controller/HelloController.java
+++ src/main/java/cn/tedu/controller/HelloController.java
@@ -2,6 +2,8 @@ package cn.tedu.controller;
 
 import java.util.List;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +16,15 @@ import cn.tedu.service.UserService;
 public class HelloController {
 	@Autowired
 	UserService userService;
+
+	private static final Logger logger=Logger.getLogger(HelloController.class);
 	
 	@RequestMapping("/find.action")
     public String find(Model model){
     	List<User> users =userService.finaAll();
     	System.out.println(users);
     	model.addAttribute("msg", users);
+    	logger.error(users);
     	return "message";
     }
 }
--- src/main/webapp/WEB-INF/message.jsp
+++ src/main/webapp/WEB-INF/message.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" isELIgnored="false" import="java.util.*" pageEncoding="utf-8"%>
 
 
@@ -8,6 +9,9 @@
   </head>
   
   <body>
+  <c:forEach items="${msg}" var="user">
+    ${user.name}
+  </c:forEach><br>
     ${msg} <br>
   </body>
 </html>
